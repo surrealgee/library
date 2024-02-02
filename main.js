1704b9c59eb5fb7d702681fc9e6d25285b4c4453
Surreal Gee <94802496+surrealgee@users.noreply.github.com>
2024-02-02 10:35:27 -0400

    Add control buttons

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
-const myLibrary = [];
+let myLibrary = [];
 
 const addBtn = document.querySelector('.add_button');
 const dialog = document.querySelector('.newBookDialog');
 const submitBtn = document.querySelector('#submit_btn');
 const cancelBtn = document.querySelector('#cancel_btn');
 const bookForm = document.querySelector('#book_form');
+const booksTable = document.querySelector('.books_table');
+
+// console.log(booksTable);
+
+booksTable.addEventListener('click', (e) => {
+    if (e.target.id === 'delete_btn') {
+        deleteBook(e.target);
+    } else if (e.target.id === 'read_btn') {
+        updateBookStatus(e.target);
+    }
+
+    displayBooks();
+})
 
 addBtn.addEventListener('click', (e) => {
     dialog.showModal();
@@ -31,8 +44,8 @@ submitBtn.addEventListener('click', e => {
 
         addBookToLibrary(newBook);
         displayBooks();
-
         dialog.close();
+        clearFields();
     }
 })
 
@@ -41,7 +54,25 @@ cancelBtn.addEventListener('click', (e) => {
     dialog.close();
 })
 
+function updateBookStatus(targetEl) {
+    for (let i = 0; i < myLibrary.length; i++) {
+        if (i == targetEl.dataset.index) {
+            myLibrary[i].isRead = !myLibrary[i].isRead;
+        }
+    }
+}
 
+function clearFields() {
+    const inputs = document.querySelectorAll('#book_form input');
+
+    for (let input of inputs) {
+        if (input.type === 'checkbox') {
+            input.checked = false;
+        } else {
+            input.value = '';
+        }
+    }
+}
 
 function Book(title, author, pages, isRead) {
     this.title = title,
@@ -62,21 +93,38 @@ function displayBooks() {
         <th>Author</th>
         <th>Pages</th>
         <th>Read?</th>
+        <th>Controls</th>
     </thead>
     `;
 
-    for (let book of myLibrary) {
+    for (let i = 0; i < myLibrary.length; i++) {
         books += `
         <tr>
-            <td>${book.title}</td>
-            <td>${book.author}</td>
-            <td>${book.pages}</td>
-            <td>${book.isRead ? 'Yes' : 'No'}</td>
+            <td>${myLibrary[i].title}</td>
+            <td>${myLibrary[i].author}</td>
+            <td>${myLibrary[i].pages}</td>
+            <td>${myLibrary[i].isRead ? 'Yes' : 'No'}</td>
+            <td class="controls">
+            <button id="read_btn" data-index=${i}>${myLibrary[i].isRead ? "Unread" : "Read"}</button>
+            <button id="delete_btn" data-index=${i}>X</button>
+            </td>
         </tr>
         `
     }
 
     booksTable.innerHTML = books;
+}
+
+function deleteBook(targetEl) {
+    const newArr = [];
+
+    for (let i = 0; i < myLibrary.length; i++) {
+        if (i != targetEl.dataset.index) {
+            newArr.push(myLibrary[i]);
+        }
+    }
+
+    myLibrary = newArr;
 }
 
 const book1 = new Book('Atomic Habits', 'James Clear', 320, true);
